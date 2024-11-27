915874a2187c2bab6174ede4351b1aab492f1864
Malyshev Alexander <_undead_@mail.ru>
2024-11-28 09:27:48 +1000

    database - сид для наполнения базы данных

--- products/database/seeders/DatabaseSeeder.php
+++ products/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Product;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Product::factory()
+            ->count(73)
+            ->create();
     }
 }
